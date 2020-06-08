613daed75e49cc21905d73f1abb4db56f34ecaf3
Vereschak <danil.vereschak@gmail.com>
2020-06-08 13:28:30 +0300

    Update change.php

--- change.php
+++ change.php
@@ -31,10 +31,10 @@ foreach ($files as $file) {
         $text =  str_replace('{{ $t(\''.$key.'\')}}', $value, $text);
         
         
-        $text =  str_replace("window.Vue.i18n.translate(\"".$key."\")", addslashes("\"".$value."\""), $text);
-        $text =  str_replace("window.Vue.i18n.translate(\"".$key."\" )", addslashes("\"".$value."\""), $text);
-        $text =  str_replace("window.Vue.i18n.translate( \"".$key."\" )", addslashes("\"".$value."\""), $text);
-        $text =  str_replace("window.Vue.i18n.translate( \"".$key."\")", addslashes("\"".$value."\""), $text);
+        $text =  str_replace("window.Vue.i18n.translate(\"".$key."\")", "\"".addslashes($value)."\"", $text);
+        $text =  str_replace("window.Vue.i18n.translate(\"".$key."\" )", "\"".addslashes($value)."\"", $text);
+        $text =  str_replace("window.Vue.i18n.translate( \"".$key."\" )", "\"".addslashes($value)."\"", $text);
+        $text =  str_replace("window.Vue.i18n.translate( \"".$key."\")", "\"".addslashes($value)."\"", $text);
   
         $text =  str_replace('{{$t("'.$key.'")}}', $value, $text);
         $text =  str_replace('{{$t("'.$key.'") }}', $value, $text);
